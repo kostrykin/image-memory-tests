9badf6d1ef1c5bd7410cab26eac78808609647d1
Leonid Kostrykin <void@evoid.de>
2025-03-08 00:06:42 +0100

    Add test workflow

---
+++ .github/update_readme.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--readme', type=str, required=True)
+parser.add_argument('--tests-output', type=str, required=True)
+args = parser.parse_args()
+
+with open(args.readme) as f:
+    readme = f.read()
+
+with open(args.tests_output) as f:
+    tests_output = f.read().strip()
+
+lines = list()
+skip = False
+
+for line in readme.split('\n'):
+    if line == '<!-- END TEST OUTPUT -->':
+        skip = False
+    if not skip:
+        lines.append(line)
+    if line == '<!-- BEGIN TEST OUTPUT -->':
+        skip = True
+        lines.append(tests_output)
+
+with open(args.readme, 'w') as f:
+    f.write('\n'.join(lines))
